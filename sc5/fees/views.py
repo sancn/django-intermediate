a27179c10137bb67b5fd370c5aa1404fa58d3a17
sancn <official.santu05@gmail.com>
2023-02-12 17:17:39 +0545

    Templates with app

---
+++ sc5/fees/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+def fees_django(request):
+    return render(request,'fees/fees1.html')
+def fees_python(request):
+    return render(request,'fees/fees2.html')
